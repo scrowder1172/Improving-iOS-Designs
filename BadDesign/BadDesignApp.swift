b4cf3a7506afc9a666e6b34036ccb09c796abebc
Scott Crowder <scrowder@gmail.com>
2024-07-02 20:04:42 -0500

    Initial Commit

---
+++ BadDesign/BadDesignApp.swift
@@ -0,0 +1,20 @@
+//
+// File: BadDesignApp.swift
+// Project: BadDesign
+// 
+// Created by SCOTT CROWDER on 7/2/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct BadDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
